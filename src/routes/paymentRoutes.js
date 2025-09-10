e7bf5fe3f734abbac875aff2aa15117a88a1245c
Dynamite107 <140146269+Dynamite107@users.noreply.github.com>
2025-09-10 09:00:57 +0530

    Update paymentRoutes.js

--- src/routes/paymentRoutes.js
+++ src/routes/paymentRoutes.js
@@ -9,9 +9,9 @@ const router_payment = express.Router();
 
 // /api/payment/create-order par request aane par createOrder function chalega.
 // 'protect' middleware yeh sunishchit karta hai ki user pehle se login ho.
-router_payment.post('/create-order', protect, createOrder);
+router_payment.post('/create-razorpay-order', protect, createOrder);
 
 // /api/payment/verify par request aane par verifyPayment function chalega.
-router_payment.post('/verify', protect, verifyPayment);
+router_payment.post('/verify-razorpay', protect, verifyPayment);
 
 export default router_payment;
